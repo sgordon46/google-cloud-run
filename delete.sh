e7523a1d00c44bc280dea4f81e7ef228bb01236e
Stephen Gordon <sgordon@paloaltonetworks.com>
2023-09-14 19:15:56 +0000

    Update delete.sh file

--- delete.sh
+++ delete.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 REPO=my-tomcat-2023
+DIR=django_hl7_rest_api
+IMAGE=my-tomcat-image-2023
 
 gcloud artifacts repositories delete $REPO --location us-central1 --quiet #--async
 
-docker rmi  $IMAGE 
-docker rmi  us-central1-docker.pkg.dev/project1-366201/$REPO/$IMAGE:latest
+docker rmi $IMAGE:latest
+docker rmi us-central1-docker.pkg.dev/project1-366201/$REPO/$IMAGE:latest
 
 
 
-#rm -rf Dockerfile app_embedded_embed_app-name.zip twistlock_defender_app_embedded.tar.gz
+rm -rf $DIR/Dockerfile $DIR/app_embedded_embed_app-name.zip $DIR/twistlock_defender_app_embedded.tar.gz
 
-#mv Dockerfile.orig Dockerfile
+mv $DIR/Dockerfile.orig $DIR/Dockerfile
 
