4c70c32933ee672d8ae0533b12352977fb5983fe
Stephen Gordon <sgordon@paloaltonetworks.com>
2023-09-16 07:40:13 -0500

    testing get token

---
+++ policy/token.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import os
+
+console_url = os.environ['PCC_API']
+access_key = os.environ['PCC_KEY']
+secret_key = os.environ['PCC_SEC']
+
+
+payload = {
+    'username': access_key,
+    'password': secret_key
+}
+
+# Generate a Token for access to Prisma Cloud Compute.
+TOKEN = requests.post(console_url+"/api/v1/authenticate",
+                      json=payload).json()['token']
+
+
+env_file = os.getenv('GITHUB_ENV')
+
+with open(env_file, "a") as myfile:
+    myfile.write("TOKEN="TOKEN)
+
+    
