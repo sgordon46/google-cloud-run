ef2b528497a8f86e73256d65e42949c8e3caac3a
Stephen Gordon <sgordon@paloaltonetworks.com>
2023-11-02 12:44:37 -0500

    some code cleanup

--- policy/create-policy.py
+++ policy/create-policy.py
@@ -3,22 +3,8 @@ import json
 import os
 
 TOKEN = os.environ['TOKEN']
-
-
 console_url = os.environ['PCC_API']
-# access_key = os.environ['PCC_KEY']
-# secret_key = os.environ['PCC_SEC']
-
 application_name = os.environ['application_name']
-
-# payload = {
-#     'username': access_key,
-#     'password': secret_key
-# }
-
-# # Generate a Token for access to Prisma Cloud Compute.
-# TOKEN = requests.post(console_url+"/api/v1/authenticate",
-#                       json=payload).json()['token']
 
 # Set Prisma Cloud Headers for Login with token
 pccHeaders = {
